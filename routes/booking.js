aa8d0e48ccebf977d384062eab16e67076162358
ThinkLink365 <liamodubhgain2003@gmail.com>
2023-12-03 16:31:29 +0000

    Final version. Added comments etc

--- routes/booking.js
+++ routes/booking.js
@@ -1,5 +1,8 @@
+//importing express library and creating a router
 var express = require('express');
 var router = express.Router();
+
+//Importing booking model
 const booking = require('../routes/models/bookingmodel');
 
 
@@ -8,6 +11,7 @@ router.get('/', function (req, res, next) {
   res.render('booking', { title: 'Booking' });
 });
 
+//Handling POST requests for creating a new booking
 router.post("/Booking", function (req, res) {
   let newBooking = new booking({
     name: req.body.name,
@@ -21,8 +25,10 @@ router.post("/Booking", function (req, res) {
     card_year: req.body.card_year,
     cvv: req.body.cvv,
   });
+  //Saving the new booking to the database
   newBooking.save()
     .then((savedBooking) => {
+      //redirecting to the view bookings page once it has been saved
       res.redirect('/booking/view');
     })
     .catch((error) => {
@@ -33,7 +39,9 @@ router.post("/Booking", function (req, res) {
 /* GET view booking page. */
 router.get('/view', async (req, res) => {
   try {
+    //get all the bookings from the database
     const bookingsfound = await booking.find();
+    //render viewbookings with those details
     res.render('viewBookings', { bookingDetails: bookingsfound, title: 'View Booking' });
   } catch (error) {
     console.error(error);
@@ -41,40 +49,46 @@ router.get('/view', async (req, res) => {
   }
 });
 
+//Handling GET requests for deleting a booking
 router.get('/delete', async (req, res,) => {
   const bookingID = req.query.bookingID;
   res.redirect(`/booking/delete/${bookingID}`)
 });
 
+//Handling GET requests for modifing a booking
 router.get('/modify', async (req, res) => {
   const bookingID = req.query.bookID;
   res.redirect(`/booking/modify/${bookingID}`)
 });
 
+//Handling GET requests for searching a booking
 router.get('/search', async (req, res) => {
   const bookingsfound = await booking.find();
   res.render('search', { bookingDetails: bookingsfound, title: 'Search' });
 });
 
+//Handling POST requests for searching a booking
 router.post('/search', async (req, res) => {
   const searchName = req.body.searchName;
-  console.log(searchName)
   res.redirect(`/booking/search/${searchName}`);
 });
 
+// Handling GET requests for displaying search results
 router.get('/search/:query', async (req, res) => {
+   // Searching for bookings with the specified name in the database
   const bookings = await booking.find({ name: req.params.query }).exec();
-  console.log(bookings)
+  // Rendering the searchedBooking page with the search results
   res.render('searchedBooking', { query: req.params.query, bookings: bookings, title: 'Searched' })
 });
 
+// Handling GET requests for modifying a specific booking
 router.get('/modify/:id', async (req, res) => {
 
   try {
+    // Finding the booking by ID in the database
     const bookings = await booking.findById(req.params.id)
-
+    // Rendering the modify page with the fetched booking details
     res.render('modify', { bookings, title: 'Modify' });
-
   } catch (error) {
     console.error(error);
     res.status(500).send('Server Error');
@@ -82,12 +96,13 @@ router.get('/modify/:id', async (req, res) => {
   }
 });
 
+// Handling POST requests for updating a booking
 router.post('/edited/:id', async (req, res) => {
   const bookingID = req.body.id;
-
   try {
+    // Updating the booking in the database with the provided data
     const updatedBooking = await booking.findByIdAndUpdate(bookingID, req.body, { new: true });
-
+    // Redirecting to the view bookings page after successful update
     res.redirect('/booking/view');
   } catch (error) {
     console.error(error);
@@ -95,9 +110,12 @@ router.post('/edited/:id', async (req, res) => {
   }
 });
 
+// Handling GET requests for deleting a specific booking
 router.get('/delete/:id', async (req, res) => {
   try {
+    // Finding the booking by ID in the database
     const bookings = await booking.findById(req.params.id);
+    // Rendering the delete page with the fetched booking details
     res.render('delete', { bookings, title: 'Delete' });
   } catch (error) {
     console.error(error);
@@ -105,8 +123,10 @@ router.get('/delete/:id', async (req, res) => {
   }
 });
 
+// Handling POST requests for removing a booking
 router.post('/removed/:id', async (req, res) => {
   try {
+    // Deleting the booking from the database by ID
     const deletedBooking = await booking.findByIdAndDelete(req.params.id);
 
     // Redirect to the view bookings page after successful deletion
@@ -117,4 +137,5 @@ router.post('/removed/:id', async (req, res) => {
   }
 });
 
+// Exporting the router for use in other parts of the application
 module.exports = router;
--- routes/models/bookingmodel.js
+++ routes/models/bookingmodel.js
@@ -1,52 +1,69 @@
+// Importing mongoose library for MongoDB interactions
 const mongoose = require('mongoose');
+
+// Creating a schema using the mongoose.Schema class
 const Schema = mongoose.Schema;
 
+// Defining the structure of the booking schema
 const bookingSchema = new Schema({
+    // Name of the person making the booking (String type, required)
     name: {
-     type: String,
-     required: true,
-  },
-   email: {
-       type: String,
-       required: true,
-   },
-   contactNumber: {
-       type:String,
-       required:true,
-   },
-   date: {
-       type: Date,
-       required: true,
-   },
-   time: {
-     type:String,
-     required: true,
-   },
-   card_name:{
-     type: String,
-     required: true,
-   },
-   card_number:{
-     type: Number,
-     required:true,
-   },
-   card_month:{
-     type:String,
-     required:true,
-   },
-   card_year:{
-     type:Number,
-     required:true,
-   },
-   cvv:{
-     type:Number,
-     required:true,
-   }
-  },
-  { 
+        type: String,
+        required: true,
+    },
+    // Email of the person making the booking (String type, required)
+    email: {
+        type: String,
+        required: true,
+    },
+    // Contact number of the person making the booking (String type, required)
+    contactNumber: {
+        type: String,
+        required: true,
+    },
+    // Date of the booking (Date type, required)
+    date: {
+        type: Date,
+        required: true,
+    },
+    // Time of the booking (String type, required)
+    time: {
+        type: String,
+        required: true,
+    },
+    // Name on the credit card used for the booking (String type, required)
+    card_name: {
+        type: String,
+        required: true,
+    },
+    // Credit card number used for the booking (Number type, required)
+    card_number: {
+        type: Number,
+        required: true,
+    },
+    // Expiry month of the credit card used for the booking (String type, required)
+    card_month: {
+        type: String,
+        required: true,
+    },
+    // Expiry year of the credit card used for the booking (Number type, required)
+    card_year: {
+        type: Number,
+        required: true,
+    },
+    // CVV (Card Verification Value) of the credit card used for the booking (Number type, required)
+    cvv: {
+        type: Number,
+        required: true,
+    }
+},
+// Additional configuration for the schema (timestamps for createdAt and updatedAt)
+{
     timestamps: true
-  }
-  );  
-  const booking = mongoose.model("booking", bookingSchema);
-  
-module.exports = booking; 
+});
+
+// Creating a model based on the booking schema
+const booking = mongoose.model("booking", bookingSchema);
+
+// Exporting the booking model to be used in other parts of the application
+module.exports = booking;
